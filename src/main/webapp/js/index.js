3bb7f72e6520f44a0d716464c3dee47224910126
Miky <wqy813853090@live.cn>
2015-12-16 01:11:07 +0800

    write the index page.

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -1,4 +1,24 @@
-/**
- * 
- */
- 
+$(document).ready(function() {
+    $('.page').bind("click", function() {
+        var id=$(this).attr('id').toString();
+        if (id=="htmlPage") {
+            location.href = "../login";
+        };
+        if (id=="jspPage") {
+            location.href = "../index";
+        };
+    });
+
+    reSetSize();
+});
+
+$(window).resize(reSetSize);
+
+function reSetSize() {
+    var winHeight = $(window).height();
+    var headerHeight = (winHeight * 0.4);
+    var bodyHeight = (winHeight * 0.6);
+    $('#header').css("height", headerHeight.toString());
+    $('#body').css("height", bodyHeight.toString());
+}
+
